763979fc5e8d857b07d60c41e37ee6579539958b
AdamP <adam.poukar@ceskesperky.cz>
2018-02-18 10:43:24 +0100

    Add comments

--- app/src/main/java/com/example/android/poukarpresentation/MainActivity.java
+++ app/src/main/java/com/example/android/poukarpresentation/MainActivity.java
@@ -26,9 +26,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+        HashMap of the information about company
+         */
         final HashMap<String, String> poukar = new HashMap<>();
         poukar.put("Company Name", "Poukar - české šperky");
-        poukar.put("Company Type", "Velkoobchod");
+        poukar.put("Company Type", getString(R.string.wholesale));
         poukar.put("Executive Head", "Ing. Aleš Poukar");
         poukar.put("Phone number", "+420 602 538 800");
         poukar.put("Email Address", "obchod@ceskesperky.cz");
@@ -36,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
         poukar.put("Address first line", "28. října 3346/91");
         poukar.put("Address second line", "702 00 Ostrava");
 
+        /*
+        HashMap of the opening times of the office
+         */
         LinkedHashMap<String, String> openingHours = new LinkedHashMap<String, String>();
         openingHours.put(getString(R.string.monday), "7:00 - 16:00");
         openingHours.put(getString(R.string.tuesday), "7:00 - 16:00");
@@ -45,6 +51,10 @@ public class MainActivity extends AppCompatActivity {
         openingHours.put(getString(R.string.saturday), "zavřeno");
         openingHours.put(getString(R.string.sunday), "zavřeno");
 
+        /*
+        Defines and sets the photo slider
+        at the top of the app
+         */
         BannerSlider bannerSlider = (BannerSlider) findViewById(R.id.banner_slider1);
         List<Banner> banners=new ArrayList<>();
         //add banner using resource drawable
@@ -55,7 +65,11 @@ public class MainActivity extends AppCompatActivity {
         }
         bannerSlider.setBanners(banners);
 
-        //Sets the
+        /*
+        Retrieves the information about the company
+        from the HashMap poukar and passes the values
+        to the relevant TextViews
+         */
         TextView companyNameTextView = (TextView) findViewById(R.id.company_name);
         companyNameTextView.setText(poukar.get("Company Name"));
 
@@ -65,6 +79,35 @@ public class MainActivity extends AppCompatActivity {
         TextView executiveHead = (TextView) findViewById(R.id.executive_head_name);
         executiveHead.setText(poukar.get("Executive Head"));
 
+        TextView phoneNumberTextView = (TextView) findViewById(R.id.phone_number);
+        phoneNumberTextView.setText(poukar.get("Phone number"));
+
+        TextView emailAddressTextView = (TextView) findViewById(R.id.email_address);
+        emailAddressTextView.setText(poukar.get("Email Address"));
+
+        TextView webAddressTextView = (TextView) findViewById(R.id.web_address);
+        webAddressTextView.setText(poukar.get("Web Address"));
+
+        TextView address = (TextView) findViewById(R.id.address);
+        address.setText(poukar.get("Address first line") +
+                "\n" + poukar.get("Address second line"));
+
+        /*
+        Loops through openingHours HashMap
+        and passes the days and opening times
+        to the TextView
+         */
+        TextView openingDaysTextView = (TextView) findViewById(R.id.opening_days);
+        TextView openingHoursTextView = (TextView) findViewById(R.id.opening_hours);
+        for (String day : openingHours.keySet()) {
+            openingDaysTextView.append(day + ":\n");
+            openingHoursTextView.append(openingHours.get(day) + "\n");
+        }
+
+        /*
+        Sets what should happen
+        after clicking on the particular layouts
+         */
         LinearLayout phoneNumberLayout = (LinearLayout) findViewById(R.id.phone_number_layout);
         phoneNumberLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,9 +115,6 @@ public class MainActivity extends AppCompatActivity {
                 dialPhoneNumber(poukar.get("Phone number"));
             }
         });
-
-            TextView phoneNumberTextView = (TextView) findViewById(R.id.phone_number);
-        phoneNumberTextView.setText(poukar.get("Phone number"));
 
         LinearLayout emailAddressLayout = (LinearLayout) findViewById(R.id.email_address_layout);
         emailAddressLayout.setOnClickListener(new View.OnClickListener() {
@@ -84,9 +124,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        TextView emailAddressTextView = (TextView) findViewById(R.id.email_address);
-        emailAddressTextView.setText(poukar.get("Email Address"));
-
         LinearLayout webAddressLayout = (LinearLayout) findViewById(R.id.web_address_layout);
         webAddressLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,16 +131,6 @@ public class MainActivity extends AppCompatActivity {
                 openWebPage(poukar.get("Web Address"));
             }
         });
-
-        TextView webAddressTextView = (TextView) findViewById(R.id.web_address);
-        webAddressTextView.setText(poukar.get("Web Address"));
-
-        TextView openingDaysTextView = (TextView) findViewById(R.id.opening_days);
-        TextView openingHoursTextView = (TextView) findViewById(R.id.opening_hours);
-        for (String day : openingHours.keySet()) {
-            openingDaysTextView.append(day + ":\n");
-            openingHoursTextView.append(openingHours.get(day) + "\n");
-        }
 
         LinearLayout addressLinearLayout = (LinearLayout) findViewById(R.id.address_layout);
         addressLinearLayout.setOnClickListener(new View.OnClickListener() {
@@ -114,17 +141,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        TextView address = (TextView) findViewById(R.id.address);
-        address.setText(poukar.get("Address first line") +
-                "\n" + poukar.get("Address second line"));
-
 
     }
 
     /*
     After calling this function it takes
-    the phone number of the object
-    and pass it to the phone dialer
+    the phone number of the poukar HashMap
+    and passes it to the phone dialer
      */
     public void dialPhoneNumber(String phoneNumber) {
         Intent intent = new Intent(Intent.ACTION_DIAL);
@@ -134,6 +157,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*
+    Opens e-mail app. Passes the email address
+    from poukar HashMap & subject from strings.xml
+     */
     public void sendEmail(String emailAddress) {
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:" + emailAddress +
@@ -143,6 +170,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*
+    Opens the web browser and passes
+    the web address from poukar HashMap
+     */
     public void openWebPage(String url) {
         Uri webpage = Uri.parse("http://" + url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
@@ -151,6 +182,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*
+    Opens the navigation app and passes
+    the address from poukar HashMap
+     */
     public void showMap(String address) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse("geo:0,0?q=" + address));
